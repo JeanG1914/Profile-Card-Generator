dcc93a35ec6f1f037e467e259b193df7c7a65117
JeanG1914 <jeanpaulgzea@gmail.com>
2022-12-17 01:18:21 +0000

    variables added

--- src/js/app.js
+++ src/js/app.js
@@ -29,14 +29,20 @@ function render(variables = {}) {
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
 
+  let background =`<dic class="background"><img src="${variables.includeCover}" /></div>`;
+  if (variables.background == false) background ="<div class='background'></div>";
+
+  let name =`<div class="Lucy Boilett">`</div>;
+  if (variables.name == null) name ="<div class='name'></div>";
+
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
+          <h1>${variables.name}</h1>
           <h2>Web Developer</h2>
           <h3>Miami, USA</h3>
-          <ul class="position-right">
+          <ul class=`position-${variables.socialMediaPosition}`>
             <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
             <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
             <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
